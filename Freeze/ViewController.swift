2c07b19559a16f7b0238ea1b341f4a25e19d633f
kdj <bonybony@photowidget.net>
2025-01-28 13:27:27 +0900

    Initial Commit

---
+++ Freeze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Freeze
+//
+//  Created by 김동준 on 1/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
